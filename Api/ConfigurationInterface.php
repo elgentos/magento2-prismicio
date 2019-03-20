36bcc4a65016c49fefba18a2b0c3b1953afb764a
Jeroen Boersma <jeroen@elgentos.nl>
2019-03-20 21:43:12 +0100

    Updated configuration model

--- Api/ConfigurationInterface.php
+++ Api/ConfigurationInterface.php
@@ -11,12 +11,14 @@ namespace Elgentos\PrismicIO\Api;
 
 use \Magento\Store\Api\Data\StoreInterface;
 
-interface Configuration
+interface ConfigurationInterface
 {
 
-    const XML_PATH_API_ENDPOINT = 'elgentos_prismicio/general/enpoint';
-    const XML_PATH_API_SECRET = 'elgentos_prismicio/general/token';
+    const XML_PATH_API_ENABLED = 'prismicio/general/enabled';
+    const XML_PATH_API_ENDPOINT = 'prismicio/general/enpoint';
+    const XML_PATH_API_SECRET = 'prismicio/general/token';
 
+    public function getApiEnabled(StoreInterface $store): bool;
     public function getApiEndpoint(StoreInterface $store): string;
     public function getApiSecret(StoreInterface $store): string;
 
--- Model/Configuration.php
+++ Model/Configuration.php
@@ -8,19 +8,19 @@
 
 namespace Elgentos\PrismicIO\Model;
 
-use Elgentos\PrismicIO\Api\Configuration as ConfigurationInterface;
-use Magento\Framework\App\Config\ReinitableConfigInterface;
+use Elgentos\PrismicIO\Api\ConfigurationInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Api\Data\StoreInterface;
 use Magento\Store\Model\ScopeInterface;
 
 class Configuration implements ConfigurationInterface
 {
 
-    /** @var ReinitableConfigInterface */
+    /** @var ScopeConfigInterface */
     private $config;
 
     public function __construct(
-        ReinitableConfigInterface $config
+        ScopeConfigInterface $config
     )
     {
         $this->config = $config;
@@ -28,12 +28,29 @@ class Configuration implements ConfigurationInterface
 
     public function getApiEndpoint(StoreInterface $store): string
     {
-        return $this->config->getValue(self::XML_PATH_API_ENDPOINT, ScopeInterface::SCOPE_STORE, $store->getCode());
+        return $this->config->getValue(
+            self::XML_PATH_API_ENDPOINT,
+            ScopeInterface::SCOPE_STORE,
+            $store
+        );
     }
 
     public function getApiSecret(StoreInterface $store): string
     {
-        return $this->config->getValue(self::XML_PATH_API_SECRET, ScopeInterface::SCOPE_STORE, $store->getCode());
+        return $this->config->getValue(
+            self::XML_PATH_API_SECRET,
+            ScopeInterface::SCOPE_STORE,
+            $store
+        );
+    }
+
+    public function getApiEnabled(StoreInterface $store): bool
+    {
+        return !! $this->config->getValue(
+            self::XML_PATH_API_ENABLED,
+            ScopeInterface::SCOPE_STORE,
+            $store
+        );;
     }
 
 }

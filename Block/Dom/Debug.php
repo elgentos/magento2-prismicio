e4967e8544e560973345b6af0df0ba281ddb4db3
Jeroen Boersma <jeroen@elgentos.nl>
2019-04-16 08:44:21 +0000

    Debug only works in developer mode

--- Block/Dom/Debug.php
+++ Block/Dom/Debug.php
@@ -8,11 +8,18 @@
 
 namespace Elgentos\PrismicIO\Block\Dom;
 
+use Magento\Framework\App\ObjectManager\Environment\Developer;
+
 class Debug extends AbstractDom
 {
 
     public function fetchDocumentView(): string
     {
+        if ($this->_appState->getMode() !== Developer::MODE) {
+            // Only allow debug in developer mode
+            return '';
+        }
+
         return '<pre>' . print_r($this->getContext(), true) . '</pre>';
     }
 

ae5e63e7a251808a08999888b27a569c2c9c7308
Jeroen Boersma <jeroen@elgentos.nl>
2019-10-10 22:39:20 +0200

    Pass content of child Prismic content to Magento\Email\Filter
    + added Magento filter directives

---
+++ Block/FilterVars.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace Elgentos\PrismicIO\Block;
+
+
+use Elgentos\PrismicIO\ViewModel\DocumentResolver;
+use Elgentos\PrismicIO\ViewModel\LinkResolver;
+use Magento\Email\Model\Template\Filter;
+use Magento\Framework\View\Element\Context;
+
+class FilterVars extends AbstractBlock
+{
+    /**
+     * @var Filter
+     */
+    private $filter;
+
+    /**
+     * MagentoVars constructor.
+     * @param Context $context
+     * @param DocumentResolver $documentResolver
+     * @param LinkResolver $linkResolver
+     * @param Filter $filter
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        DocumentResolver $documentResolver,
+        LinkResolver $linkResolver,
+        Filter $filter,
+        array $data = []
+    ) {
+        parent::__construct($context, $documentResolver, $linkResolver, $data);
+        $this->filter = $filter;
+    }
+
+    /**
+     * Fetch document view
+     *
+     * @return string
+     */
+    public function fetchDocumentView(): string
+    {
+        foreach ($this->getChildNames() as $alias) {
+            $this->updateChildDocument($alias);
+        }
+
+        // Replace
+        $html = preg_replace('/{{([^ }]+)}}/', '{{customvar code=\\1}}', $this->getChildHtml());
+        return $this->filter->filter($html);
+    }
+}
+

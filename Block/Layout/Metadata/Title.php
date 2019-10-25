7990d26a3ef48d6c67005a612988a8ab12570262
Wouter Steenmeijer <wouter@elgentos.nl>
2019-10-25 10:16:33 +0200

    Added meta title and description blocks

---
+++ Block/Layout/Metadata/Title.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: jeroen
+ * Date: 15-4-19
+ * Time: 23:39
+ */
+
+namespace Elgentos\PrismicIO\Block\Layout\Metadata;
+
+use Elgentos\PrismicIO\Block\AbstractTemplate;
+
+class Title extends AbstractTemplate
+{
+    protected function _prepareLayout()
+    {
+        $this->pageConfig->setMetadata('title',  $this->getChildHtml());
+
+        return $this;
+    }
+}

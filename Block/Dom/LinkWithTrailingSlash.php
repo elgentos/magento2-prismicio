f0b0dc64afdb0c5aa9e1639a5fa61e7e8095968e
Jeroen Boersma <jeroen@elgentos.nl>
2019-10-25 15:14:22 +0200

    Added Dom/Block for trailing slash

---
+++ Block/Dom/LinkWithTrailingSlash.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace Elgentos\PrismicIO\Block\Dom;
+
+class LinkWithTrailingSlash extends Link
+{
+    public function fetchDocumentView(): string
+    {
+        return $this->escapeUrl(parent::fetchDocumentView() . '/');
+    }
+}

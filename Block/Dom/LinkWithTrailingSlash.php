3ed87c30c27a6e8765e77680cf3b9e7b48c3b99d
Jeroen Boersma <jeroen@elgentos.nl>
2020-07-02 23:31:19 +0200

    Added a LinkResolver plugin to add trailing slashes
    - deprecated LinkWithTrailingSlash
    + added plugin which can watch the link resolver
    + added xml stub

--- Block/Dom/LinkWithTrailingSlash.php
+++ Block/Dom/LinkWithTrailingSlash.php
@@ -2,6 +2,13 @@
 
 namespace Elgentos\PrismicIO\Block\Dom;
 
+/**
+ * Class LinkWithTrailingSlash
+ * @package Elgentos\PrismicIO\Block\Dom
+ *
+ * @deprecated we have deprecated this feature because you can now just add a plugin which adds these trailing slashes for all Prismic links
+ * @see \Elgentos\PrismicIO\Plugin\ViewModel\LinkResolver\AppendTrailingSlashes
+ */
 class LinkWithTrailingSlash extends Link
 {
     public function fetchDocumentView(): string

09faae1f5807b6178eaf7477c91977b8611d1668
Jeroen Boersma <jeroen@elgentos.nl>
2020-07-03 11:21:40 +0200

    Add a rich text serializer to create the possibility to add plugins

--- Block/Dom/RichText.php
+++ Block/Dom/RichText.php
@@ -9,12 +9,32 @@
 namespace Elgentos\PrismicIO\Block\Dom;
 
 use Elgentos\PrismicIO\Block\AbstractBlock;
+use Elgentos\PrismicIO\ViewModel\DocumentResolver;
+use Elgentos\PrismicIO\ViewModel\HtmlSerializer;
+use Elgentos\PrismicIO\ViewModel\LinkResolver;
+use Magento\Framework\View\Element\Context;
 use Prismic\Dom\RichText as PrismicRichText;
 
 class RichText extends AbstractBlock
 {
+    /**
+     * @var HtmlSerializer
+     */
+    private $htmlSerializer;
+
+    public function __construct(
+        Context $context,
+        DocumentResolver $documentResolver,
+        LinkResolver $linkResolver,
+        HtmlSerializer $htmlSerializer,
+        array $data = []
+    ) {
+        parent::__construct($context, $documentResolver, $linkResolver, $data);
+        $this->htmlSerializer = $htmlSerializer;
+    }
+
     public function fetchDocumentView(): string
     {
-        return PrismicRichText::asHtml($this->getContext(), $this->getLinkResolver());
+        return PrismicRichText::asHtml($this->getContext(), $this->getLinkResolver(), [$this->htmlSerializer, 'serialize']);
     }
 }

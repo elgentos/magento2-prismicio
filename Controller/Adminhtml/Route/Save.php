8937b306fd2d6ced596d0387c8d6d0ff91e84840
Peter Jaap Blaakmeer <peterjaap@elgentos.nl>
2021-01-06 16:45:03 +0100

    First try at creating forms for routes

---
+++ Controller/Adminhtml/Route/Save.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Elgentos\PrismicIO\Controller\Adminhtml\Route;
+
+use Magento\Framework\Exception\LocalizedException;
+
+class Save extends \Magento\Backend\App\Action
+{
+
+    protected $dataPersistor;
+
+    /**
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\App\Request\DataPersistorInterface $dataPersistor
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\App\Request\DataPersistorInterface $dataPersistor
+    ) {
+        $this->dataPersistor = $dataPersistor;
+        parent::__construct($context);
+    }
+
+    /**
+     * Save action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        $data = $this->getRequest()->getPostValue();
+        if ($data) {
+            $id = $this->getRequest()->getParam('route_id');
+
+            $model = $this->_objectManager->create(\Elgentos\PrismicIO\Model\Route::class)->load($id);
+            if (!$model->getId() && $id) {
+                $this->messageManager->addErrorMessage(__('This Route no longer exists.'));
+                return $resultRedirect->setPath('*/*/');
+            }
+
+            $model->setData($data);
+
+            try {
+                $model->save();
+                $this->messageManager->addSuccessMessage(__('You saved the Route.'));
+                $this->dataPersistor->clear('prismicio_route');
+
+                if ($this->getRequest()->getParam('back')) {
+                    return $resultRedirect->setPath('*/*/edit', ['route_id' => $model->getId()]);
+                }
+                return $resultRedirect->setPath('*/*/');
+            } catch (LocalizedException $e) {
+                $this->messageManager->addErrorMessage($e->getMessage());
+            } catch (\Exception $e) {
+                $this->messageManager->addExceptionMessage($e, __('Something went wrong while saving the Route.'));
+            }
+
+            $this->dataPersistor->set('prismicio_route', $data);
+            return $resultRedirect->setPath('*/*/edit', ['route_id' => $this->getRequest()->getParam('route_id')]);
+        }
+        return $resultRedirect->setPath('*/*/');
+    }
+}
+
--- Model/Route/Store.php
+++ Model/Route/Store.php
@@ -3,7 +3,7 @@ namespace Elgentos\PrismicIO\Model\Route;
 
 class Store extends \Magento\Framework\Model\AbstractModel implements \Elgentos\PrismicIO\Api\Data\Route\StoreInterface, \Magento\Framework\DataObject\IdentityInterface
 {
-    const CACHE_TAG = 'elgentos_prismicio_route_store';
+    const CACHE_TAG = 'prismicio_route_store';
 
     protected function _construct()
     {

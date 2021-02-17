24474e213ce2aa1b35d73a9a202ae903ffef9037
Arjen Miedema <arjen@elgentos.nl>
2021-02-17 15:37:35 +0100

    First implementation of unit tests

---
+++ tests/Block/Dom/ClickableLinkTest.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Copyright - elgentos ecommerce solutions (https://elgentos.nl)
+ */
+
+declare(strict_types=1);
+
+namespace Elgentos\PrismicIO\Tests\Block\Dom;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \Elgentos\PrismicIO\BLock\Dom\ClickableLink
+ */
+class ClickableLinkTest extends TestCase
+{
+}

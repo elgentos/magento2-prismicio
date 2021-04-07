f6c956213dbc2c6e9d1115025b998f0a8813171f
Jeroen Boersma <jeroen@elgentos.nl>
2021-04-07 12:54:39 +0200

    Allow loading static blocks from prismic
    ```xml
    <block class="Elgentos\PrismicIO\Block\StaticBlock">
        <arguments>
            <argument name="data" xsi:type="array">
                <item name="identifier" xsi:type="string">uid-in-prismicio</item>
                <item name="type" xsi:type="string">content-type-in-prismicio</item>
            </argument>
        </arguments>
    </block>
    
    ```

---
+++ Block/StaticBlock.php
@@ -0,0 +1,130 @@
+<?php
+
+
+namespace Elgentos\PrismicIO\Block;
+
+use Elgentos\PrismicIO\Exception\ContextNotFoundException;
+use Elgentos\PrismicIO\Exception\DocumentNotFoundException;
+use Elgentos\PrismicIO\Model\Api;
+use Elgentos\PrismicIO\ViewModel\DocumentResolver;
+use Elgentos\PrismicIO\ViewModel\LinkResolver;
+use Magento\Framework\View\Element\Context;
+use stdClass;
+
+
+class StaticBlock extends AbstractBlock
+{
+    /** @var Api */
+    private $api;
+    /** @var string */
+    private $contentType;
+    /**
+     * @var string|null
+     */
+    private $identifier;
+
+    /**
+     * Constructor.
+     *
+     * @param Context $context
+     * @param DocumentResolver $documentResolver
+     * @param LinkResolver $linkResolver
+     * @param Api $api
+     * @param string $contentType
+     * @param string|null $identifier
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        DocumentResolver $documentResolver,
+        LinkResolver $linkResolver,
+        Api $api,
+        string $contentType = 'static_block',
+        string $identifier = null,
+        array $data = []
+    ) {
+        parent::__construct($context, $documentResolver, $linkResolver, $data);
+        $this->api = $api;
+        $this->contentType = $contentType;
+        $this->identifier = $identifier;
+    }
+
+    /**
+     * @return string
+     */
+    protected function _toHtml(): string
+    {
+        $this->createPrismicDocument();
+        return parent::_toHtml();
+    }
+
+    /**
+     * @return void
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    private function createPrismicDocument()
+    {
+        $data = $this->getData('data') ?? [];
+        if (! (isset($this->contentType, $this->identifier) || isset($data['uid']) || isset($data['identifier']))) {
+            return;
+        }
+
+        $document = new stdClass();
+        $options  = $this->api->getOptions();
+
+        $document->uid  = $data['uid'] ?? $data['identifier'] ?? $this->identifier;
+        $document->type = $data['content_type'] ?? $this->contentType;
+        $document->lang = $data['lang'] ??  $options['lang'];
+
+        $this->setDocument($document);
+    }
+
+    /**
+     * @return string
+     * @throws ContextNotFoundException
+     * @throws DocumentNotFoundException
+     */
+    public function fetchDocumentView(): string
+    {
+        if (! $this->fetchChildDocument()) {
+            return '';
+        }
+
+        $html = '';
+        foreach ($this->getChildNames() as $childName) {
+            $useCache = ! $this->updateChildDocumentWithDocument($childName);
+            $html    .= $this->getChildHtml($childName, $useCache);
+        }
+
+        return $html;
+    }
+
+    /**
+     * @return bool
+     * @throws \Elgentos\PrismicIO\Exception\ApiNotEnabledException
+     * @throws ContextNotFoundException
+     * @throws DocumentNotFoundException
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    private function fetchChildDocument(): bool
+    {
+        $context = $this->getContext();
+
+        // We need to update the document to the current context to change scope for children
+        $this->setDocument($context);
+
+        $uid  = $context->uid ?? '';
+        $type = $context->type ?? '';
+
+        $document = $this->api->getDocumentByUid($uid, $type, ['lang' => $context->lang]);
+        if (! $document) {
+            return false;
+        }
+
+        // Needed to correctly resolve url's
+        $document->link_type = 'Document';
+        $this->setDocument($document);
+
+        return true;
+    }
+}

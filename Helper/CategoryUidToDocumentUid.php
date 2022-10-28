c739063d804b497f88bbae75a0efbe20634c870b
Jesse de Boer <jesse@elgentos.nl>
2022-10-28 14:38:16 +0200

    Added a helper to get the current category

---
+++ Helper/CategoryUidToDocumentUid.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Elgentos\PrismicIO\Helper;
+
+// This is a helper that you can use to get the UID of a document from a category ID.
+
+class CategoryIdToDocumentUid {
+    const PREFIX = 'category-';
+
+    public function getUid(): string {
+        return self::PREFIX . $this->layerResolver->get()->getCurrentCategory()->getId() ?? '';
+    }
+
+}

b416b5e4c998464a1155c596615919024576eb10
Jesse de Boer <jesse@frsc.nl>
2022-12-05 13:55:39 +0100

    Updated Link to also work with strings

--- Block/Dom/Link.php
+++ Block/Dom/Link.php
@@ -17,8 +17,9 @@ class Link extends AbstractBlock
     {
         $context = $this->getContext();
         if (!isset($context->link_type)) {
-            $context->link_type = 'Document';
+            return $this->escapeUrl($this->replaceRelativeUrl($url));
         }
+        
         $url = PrismicLink::asUrl($context, $this->getLinkResolver() ?? '');
 
         if(!$url) {

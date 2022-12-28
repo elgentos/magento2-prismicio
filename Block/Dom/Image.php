5752dec8506f67c52724d0d228e4eb87963b8f17
Jesse de Boer <jesse@frsc.nl>
2022-12-28 11:48:17 +0100

    Better Image when things are not available
    Return null if there is no url available
    Return empty string when there is no alt available

--- Block/Dom/Image.php
+++ Block/Dom/Image.php
@@ -9,13 +9,17 @@ class Image extends AbstractBlock
     public function fetchDocumentView(): string
     {
         $context = $this->getContext();
+        
+        if (!isset($context->url)) {
+            return null;
+        }
 
         $cssClasses = $this->getData('css_class') ? 'class="'. $this->getData('css_class') .'"' : '';
-
+        
         return sprintf(
             '<img src="%s" alt="%s" width="%d" height="%d" %s />',
             $context->url,
-            $context->alt,
+            $context->alt ?? '',
             $context->dimensions->width,
             $context->dimensions->height,
             $cssClasses

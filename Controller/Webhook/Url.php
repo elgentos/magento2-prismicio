3f6038531de613df86b75784853b5bb0f0dee145
Peter Jaap Blaakmeer <peterjaap@elgentos.nl>
2023-02-03 20:05:45 +0100

    Fix route protection in Url controller

--- Controller/Webhook/Url.php
+++ Controller/Webhook/Url.php
@@ -72,10 +72,6 @@ class Url implements HttpPostActionInterface, CsrfAwareActionInterface
 
     public function execute(): ?ResultInterface
     {
-        if (!$this->protectRoute()) {
-            return null;
-        }
-
         $result = $this->resultFactory->create(ResultFactory::TYPE_JSON);
 
         $payload = json_decode($this->request->getContent() ?? '', true);
@@ -83,6 +79,10 @@ class Url implements HttpPostActionInterface, CsrfAwareActionInterface
             return $result->setData([
                 'success' => true
             ]);
+        }
+
+        if (!$this->protectRoute($payload)) {
+            return null;
         }
 
         $store = $this->storeManager->getStore();
@@ -165,12 +165,14 @@ class Url implements HttpPostActionInterface, CsrfAwareActionInterface
         return true;
     }
 
-    private function protectRoute()
+    private function protectRoute(array $payload): bool
     {
         $accessToken = $this->configuration->getWebhookSecret($this->storeManager->getStore());
 
-        if ($this->request->getParam('secret') === $accessToken) {
+        if ($payload['secret'] ?? '' === $accessToken) {
             return true;
         }
+
+        return false;
     }
 }

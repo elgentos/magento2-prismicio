4e417717ce7210c15b95be4008da2a912f8cc480
Jeroen Boersma <jeroen@elgentos.nl>
2023-02-16 22:09:15 +0100

    Canonical/AlternateLanguage
    + check if store has Prismic enabled on alternate links
    + clone link object before resolving url
    - remove unused uses

--- Block/Canonical.php
+++ Block/Canonical.php
@@ -2,12 +2,6 @@
 
 namespace Elgentos\PrismicIO\Block;
 
-use Elgentos\PrismicIO\Api\ConfigurationInterface;
-use Elgentos\PrismicIO\ViewModel\DocumentResolver;
-use Elgentos\PrismicIO\ViewModel\LinkResolver;
-use Magento\Framework\View\Element\Template;
-use Magento\Store\Model\StoreManagerInterface;
-
 class Canonical extends AbstractTemplate
 {
     public function getCanonicalUrl(): string
@@ -17,9 +11,9 @@ class Canonical extends AbstractTemplate
 
     public function getCanonical(): array
     {
-        $link = $this->getContext();
-
+        $link = clone $this->getContext();
         $link->link_type = 'Document';
+
         $href = $this->getLinkResolver()
             ->resolve($link);
 
